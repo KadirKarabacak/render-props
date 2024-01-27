2da21e08dc19a0d4880eed59aab33a63557e1c8a
Kadir <kadirht@hotmail.com>
2024-01-27 23:34:34 +0300

    chore: nothing important

--- src/HOC.jsx
+++ src/HOC.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function withToggles(WrappedComponent) {
     return function List(props) {
