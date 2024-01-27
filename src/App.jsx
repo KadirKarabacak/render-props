1c3d709eec496f25f898a41ae68159aac913982b
Kadir <kadirht@hotmail.com>
2024-01-27 23:34:12 +0300

    feat: render props feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
+import withToggles from "./HOC";
 import "./styles.css";
 
 // Random products and companies
@@ -79,11 +80,25 @@ function List({ title, items, render }) {
     );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+    return (
+        <ul className="list">
+            {items.map(product => (
+                <ProductItem key={product.productName} product={product} />
+            ))}
+        </ul>
+    );
+}
+
+// Higher order component
+const ProductListWithToggles = withToggles(ProductList);
+
 export default function App() {
     return (
         <div>
             <h1>Render Props Demo</h1>
-            <div className="col-2">
+            {/* <div className="col-2">
                 <List
                     title="Products"
                     items={products}
@@ -108,18 +123,12 @@ export default function App() {
                         />
                     )}
                 />
+            </div> */}
+
+            <div className="col-2">
+                <ProductList title="Products HOC" items={products} />
+                <ProductListWithToggles title="Products HOC" items={products} />
             </div>
         </div>
-    );
-}
-
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-    return (
-        <ul className="list">
-            {items.map(product => (
-                <ProductItem key={product.productName} product={product} />
-            ))}
-        </ul>
     );
 }
